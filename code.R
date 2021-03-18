18c728671dde940491770131826ce1c72f121faf
DylanBaptiste <dylan.baptiste@etudiant.univ-reims.fr>
2021-03-18 17:42:38 +0100

    mise en ligne

---
+++ code.R
@@ -0,0 +1,117 @@
+install.packages(c('ggplot2', 'dplyr', 'lubridate', 'plotly'))
+
+library("ggplot2")
+library("dplyr")
+library("lubridate")
+library("plotly")
+
+# ================================== ======= ================================== #
+# ================================== PLOT OK ================================== #
+# ================================== ======= ================================== #
+
+
+# ================================== ==================== ================================== #
+# ================================== PLOT EN CONSTRCUTION ================================== #
+# ================================== ==================== ================================== #
+
+usagers <- read.csv("usagers-2019.csv", sep=';')
+str(usagers)
+View(usagers)
+
+ggplot(usagers, aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Nombre et âge des personnes impliquées dans un accident de la route en France en 2019")
+
+ggplot(usagers[usagers$place==1,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Nombre et âge des conducteurs impliqués dans un accident de la route en France en 2019")
+
+ggplot(usagers[usagers$place!=1,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Nombre et âge des non conducteurs impliqués dans un accident de la route en France en 2019")
+
+ggplot(usagers[usagers$place==2,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Nombre et âge des passagé avant ou moto impliqués dans un accident de la route en France en 2019")
+
+ggplot(usagers[usagers$place==10,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("place inconnu (10 ?)")
+
+
+ggplot(usagers[usagers$catu==1,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Conducteur")
+ggplot(usagers[usagers$catu==3,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Pietons")
+
+
+labeller.catu <- function(variable, value){ return (list("1"="Indemne", "2"="Tué", "3"="Blessé hospitalisé", "4"="Blessé léger")[value])}
+ggplot(usagers[usagers$catu==3,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(vars(grav, sexe), nrow = 4, labeller=labeller.catu ) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 10)) + ggtitle("")
+ggplot(usagers[usagers$catu==3,], aes(x=2019-an_nais)) + geom_bar() + facet_wrap(vars(grav, sexe), nrow = 4) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("")
+
+library(maps)
+france<-map(database="france")
+
+dpt2001<-c("Ain","Marne","Nord","Charente")
+col2001<-c(1,2,3,5)
+match <- match.map(france,dpt2001,exact=TRUE)
+color <- col2001[match] 
+map(database="france", fill=TRUE, col=color)
+
+
+
+
+
+
+
+
+caracteristiques <- read.csv("clean_datasets/caracteristiques.csv", sep=',', header = TRUE)
+
+
+
+str(caracteristiques)
+d1 <- setNames(data.frame(table(as.Date(paste(caracteristiques$an, caracteristiques$mois, caracteristiques$jour, sep='-')))),c("Date","Count"))
+d1$day <- weekdays(as.Date(d1$Date))
+
+d2 <- d1 %>%
+	mutate(Mois=month(Date), Date = as.Date(paste(year(Date), month(Date), '01', sep = '-'))) %>%
+	group_by(Date, Mois) %>%
+	summarise(Count = sum(Count))
+
+d3 <- d1 %>%
+	mutate(Date = month(Date)) %>%
+	group_by(Date) %>%
+	summarise(Count = sum(Count))
+
+ggplotly(ggplot(d2, aes(x=Date, y=Count)) + geom_line() + geom_smooth(formula=y~x, method = 'loess'))
+plot_ly(d3, x=~Date) %>% add_lines(y=~Count)
+plot_ly(d2, x=~factor(month.abb[Mois], levels = month.abb)) %>% add_boxplot(y=~Count)
+plot_ly(d1, x=~day) %>% add_boxplot(y=~Count) %>% layout(xaxis = list(categoryorder = "array", categoryarray = c("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche")))
+
+plot_ly(d1, x=~day(Date)) %>% add_boxplot(y=~Count)
+
+d4 <- d1 %>%
+	mutate(Mois=month(Date), Day=day(Date), p=paste(month(Date), day(Date)), Date = as.Date(paste(year(Date), month(Date), day(Date), sep = '-'))) %>%
+	group_by(Day, Mois, p, Date) %>%
+	summarise(Count = sum(Count))
+d4 <- d4[ with(d4, order(Mois, Day)), ]
+plot_ly(d4, x=~p) %>% add_boxplot(y=~Count) %>% layout(xaxis = list(categoryorder = "array", categoryarray =d4$p))
+
+ggplot(caracteristiques, aes(x=dep)) + geom_bar()
+
+str(d1)
+ggplot(d, aes(x=Date, y=Count)) + geom_line()
+ggplot(caracteristiques, aes(x=an)) + geom_bar()
+ggplot(caracteristiques, aes(x=mois)) + geom_bar()
+ggplot(caracteristiques, aes(x=jour)) + geom_bar()
+ggplot(caracteristiques, aes(x=dep)) + geom_bar()
+
+ggplot(caracteristiques[caracteristiques$an==2019,], aes(x=as.Date(paste(an, mois, jour, sep='-')))) + geom_bar()
+
+usagers <- read.csv("clean_datasets/usagers.csv", sep=',', header = TRUE)
+
+str(usagers)
+ggplotly(ggplot(usagers, aes(x=an-an_nais)) + geom_bar() + facet_wrap(~sexe) + scale_x_continuous(name="âge", limits=c(0, 100), breaks = seq(0, 100, 5)) + ggtitle("Nombre et âge des personnes impliquées dans un accident de la route en France en 2019"))
+
+d <- setNames(data.frame(table(usagers$place)), c('place', 'count'))
+plot_ly(d, x=~place) %>% add_bars(d, y=~count, text=~paste0(formatC(100 * count/sum(count), format='f', digits = 2), "%"), textposition = 'auto')
+
+plot_ly(d, x=~place) %>% add_bars(d, y=~count, text=~paste0(formatC(100 * count/sum(count), format='f', digits = 2), "%"), textposition = 'auto')
+
+d <- setNames(data.frame(table(usagers$place, usagers$sexe, usagers$an)), c('place', "sexe", "an", 'count'))
+plot_ly(d, x=~place) %>% add_bars(d, y=~count, text=~sexe, textposition = 'auto') %>% layout(yaxis = list(title = 'Count'), barmode = 'group')
+
+
+
+
+
+
+
---
+++ preparation.R
@@ -0,0 +1,81 @@
+library(dplyr)
+library(stringr)
+require(tidyr)
+
+c <- read.csv("datasets/caracteristiques_2019.csv", sep=';', header = TRUE)
+l <- read.csv("datasets/lieux_2019.csv", sep=';', header = TRUE)
+u <- read.csv("datasets/usagers_2019.csv", sep=';', header = TRUE)
+v <- read.csv("datasets/vehicules_2019.csv", sep=';', header = TRUE)
+
+
+types <- c("caracteristiques", "lieux", "usagers", "vehicules")
+annees <- 2005:2019
+
+data <- list("caracteristiques"=data.frame(matrix(ncol = length(names(c))+1, nrow = 0)), "lieux"=data.frame(matrix(ncol = length(names(l)), nrow = 0)), "usagers"=data.frame(matrix(ncol = 16, nrow = 0)), "vehicules"=data.frame(matrix(ncol = length(names(v)), nrow = 0)))
+colnames(data$caracteristiques) <- c(names(c), 'gps') # gps a été effacé en 2019
+colnames(data$lieux) <- names(l) # tout ce qui est dans 2005 est deja dans 2019
+colnames(data$usagers) <- c("Num_Acc","id_vehicule","num_veh","place","catu","grav","sexe","an_nais","trajet","locp","actp","etatp","secu","secu1","secu2","secu3") # secu été divié en 3 en 2019 et id_vehicule est apparu en 2019
+colnames(data$vehicules) <- names(v) # tout ce qui est dans 2005 est deja dans 2019
+
+for(type in types){
+	for(annee in annees){
+		path <- paste0('datasets/', type, '_', annee, '.csv')
+		
+		sep <- if(annee==2019) ';' else ','
+		if(type == "caracteristiques" && annee == 2009)
+			sep = ';'
+		
+		print(paste("Process: ", type, annee))
+		nextdata <- read.csv(path, sep=sep, header = TRUE)
+		nextdata$an <- annee
+		if(type == "caracteristiques"){
+			nextdata$long <- as.numeric(nextdata$long)
+			nextdata$lat <- as.numeric(nextdata$lat)
+		}
+		if(type == "caracteristiques" && annee != 2019){
+			nextdata[(nextdata$dep < 971) & (nextdata$dep != 201) & (nextdata$dep != 202),]$dep  <- nextdata[(nextdata$dep < 971) & (nextdata$dep != 201) & (nextdata$dep != 202),]$dep / 10
+			nextdata$dep[nextdata$dep == 201] <- '2A'
+			nextdata$dep[nextdata$dep == 202] <- '2B'
+			
+		}
+		if(type == "caracteristiques" && annee == 2019){
+		
+			nextdata$com <- as.integer(gsub('^.{2}', '', nextdata$com))
+		}
+		if(type=="lieux"){
+			nextdata$voie <- as.character(nextdata$voie)
+		}
+		if(type=="lieux"){
+			nextdata$voie <- as.character(nextdata$voie)
+			nextdata$pr <- gsub("\\(|\\)", "", as.character(nextdata$pr))
+			nextdata$pr1 <- gsub("\\(|\\)", "", as.character(nextdata$pr1))
+		}
+		if(type=="usagers"){
+			nextdata$actp <- as.character(nextdata$actp)
+		}
+		data[[type]] <- dplyr::bind_rows(data[[type]], nextdata) #permet un fusion par ligne avec NA pour les colonne manquantes
+	
+	}
+}
+
+# Netoyage et ameliorations
+
+# separation de hrmn en hr, mn
+data$caracteristiques$hrmn
+data$caracteristiques$mn <- data$caracteristiques$hrmn %% 100 # garder les 2 derniers digits
+data$caracteristiques$hr <- as.integer(gsub('.{2}$', '', data$caracteristiques$hrmn)) # supprimer les 2 dernier digit
+data$caracteristiques$hr[is.na(data$caracteristiques$hr)] <- 0
+#data$caracteristiques[,c('hrmn', 'hr', 'mn')] #pour verifier
+data$caracteristiques$hrmn <- NULL
+
+
+
+write.csv(data$caracteristiques, "caracteristiques.csv", sep=",", row.names=FALSE)
+write.csv(data$lieux, "lieux.csv", sep=",", row.names=FALSE)
+write.csv(data$usagers, "usagers.csv", sep=",", row.names=FALSE)
+write.csv(data$vehicules, "vehicules.csv", sep=",", row.names=FALSE)
+
+
+
+
+
